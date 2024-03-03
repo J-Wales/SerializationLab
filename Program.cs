2d8ae5d9439e7a3cf8da12f39ba20201d2855063
J-Wales <j.wales@edu.sait.ca>
2024-03-03 11:35:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SerializationLab
+{
+	internal class Program
+	{
+		public void ReadFromFile()
+		{
+
+		}
+		static void Main(string[] args)
+		{
+			Event e = new Event();
+			e.EventNumber = 1;
+			e.Location = "Calgary";
+
+			string fileName = "event.json";
+			string j = JsonSerializer.Serialize(e);
+
+			File.WriteAllText(fileName, j);
+		}
+	}
+}
